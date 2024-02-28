64573497c728f25265648ab5a3c40a933c3cb21f
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-02-27 18:36:48 -0700

    Fixed the server connection, working on mongodb connection

--- config/connection.js
+++ config/connection.js
@@ -2,4 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/socialnetworkDB');
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/connection");
-const { User } = require("./models");
+const { User } = require("./models/Users");
+const { Thought } = require("./models/Thought");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
