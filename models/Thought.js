4897eaa6d5b397300a1aa5a7daed51e1f116ad74
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-02-27 21:30:58 -0700

    Added models for thoughts, reactions, and users

---
+++ models/Thought.js
@@ -0,0 +1,47 @@
+const { Schema, model} = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+);
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
