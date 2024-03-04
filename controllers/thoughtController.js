f43b497fc279b4a58cc9439af5428eb943c56dac
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-03-03 17:18:40 -0700

    All thought routes working:

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -45,9 +45,47 @@ module.exports = {
       }
 
       res.json(thought);
-    } catch (error) {
+    } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
+
+  async updateThought(req,res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId},
+        { $set: req.body},
+        { runValidators: true, new: true}
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with this ID!"});
+      }
+
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteThought(req,res) {
+    try {
+      const deletedThought = await Thought.findOneAndDelete(
+        { _id: req.params.thoughtId},
+        { runValidators: true, new: true}
+      );
+
+      if (!deletedThought) {
+        return res.status(404).json({ message: "No thought with this ID!"})
+      }
+
+      res.json(deletedThought);
+      
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 
-const { getThoughts, createThought, getSingleThought } = require("../../controllers");
+const {
+  getThoughts,
+  createThought,
+  getSingleThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers");
 
 //  /api/thoughts/
 router.route("/").get(getThoughts).post(createThought);
 
 //  api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought);
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 module.exports = router;
